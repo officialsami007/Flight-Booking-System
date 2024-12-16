6a608f47e8bd2a2685a80ddc8b88325d853281cb
Sami_1710 <nintex22@gmail.com>
2024-12-16 17:06:50 +0800

    - Changed the name of directory of the application run file

--- src/main/java/com/example/Flight_Booking_System/FlightBookingSystemApplication.java
+++ src/main/java/com/example/Flight_Booking_System/FlightBookingSystemApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FlightBookingSystem4Application {
+public class FlightBookingSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FlightBookingSystem4Application.class, args);
+		SpringApplication.run(FlightBookingSystemApplication.class, args);
 	}
 
 }
--- src/test/java/com/example/Flight_Booking_System/FlightBookingSystemApplicationTests.java
+++ src/test/java/com/example/Flight_Booking_System/FlightBookingSystemApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FlightBookingSystem4ApplicationTests {
+class FlightBookingSystemApplicationTests {
 
 	@Test
 	void contextLoads() {
